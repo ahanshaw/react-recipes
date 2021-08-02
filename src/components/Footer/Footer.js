36c9e187369fc17a2daf604eaeb31b1a3911a8ff
Angie Hanshaw <ahanshaw@cliquestudios.com>
2021-08-02 11:25:22 -0500

    add scss, header, footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+		<footer className="footer">
+			<p>Recipes &copy; 2021</p>
+        </footer>
+    );
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+		<header className="header">
+			<h1>
+				<a href="/">Recipes</a>
+			</h1>
+        </header>
+    );
+}
