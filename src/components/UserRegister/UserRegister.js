e383b11324ffac9210234e7d1f805592318eb6d3
Angie Hanshaw <ahanshaw@cliquestudios.com>
2021-08-13 11:52:16 -0500

    Create UserRegister.js

---
+++ src/components/UserRegister/UserRegister.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import database from '../../services/firebase';
+
+const UserRegister = () => {
+
+	return (
+		<div className="user">
+
+		</div>
+	);
+}
+
+export default UserRegister;
