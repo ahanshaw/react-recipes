9434cfdce7b61d0d0a1827eebcf21be53840ed90
Angie Hanshaw <ahanshaw@cliquestudios.com>
2021-11-15 21:10:45 -0600

    update recipe layout

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -29,43 +29,48 @@ const Recipe = () => {
 	
 	return (
 		<div className="recipe">
-			<div className="recipe-list">
-				<h1>{recipe.title}</h1>
-				{user && user.uid === recipe.user &&
-					<Link to={`/edit/${recipe.key}/${recipe.title.toLowerCase().replace(/\s/g, '-')}`}>Edit Recipe</Link>
-				}
-				
-				<p className="recipe__category">Category: <Link to={`/category/${recipe.category.toLowerCase().replace(/\s/g, '-')}`}>{recipe.category}</Link></p>
-				<p className="recipe__tags">Tags: {recipe.tags.map((tag, index) => {
-					return (
-						<Link key={index} to={`/tag/${tag.tag.toLowerCase().replace(/\s/g, '-')}`}>{tag.tag}</Link>
-					)
-				})}</p>
-				<p>Servings: {recipe.servings}</p>
-				<h2>Ingredients</h2>
-				<table className="recipe__ingredients">
-					<tbody>
-						{recipe.ingredients.map((ingredient, index) => {
+			<div className="recipe__container">
+				<div className="recipe__title">
+					<h1>{recipe.title}</h1>
+				</div>
+				<div className="recipe__cats">
+					<p>Servings: {recipe.servings}</p>					
+					<p className="recipe__category">Category: <Link to={`/category/${recipe.category.toLowerCase().replace(/\s/g, '-')}`}>{recipe.category}</Link></p>
+					<p className="recipe__tags">Tags: {recipe.tags.map((tag, index) => {
+						return (
+							<Link key={index} to={`/tag/${tag.tag.toLowerCase().replace(/\s/g, '-')}`}>{tag.tag}</Link>
+						)
+					})}</p>
+					{user && user.uid === recipe.user &&
+						<p><Link to={`/edit/${recipe.key}/${recipe.title.toLowerCase().replace(/\s/g, '-')}`}>Edit Recipe</Link></p>
+					}
+				</div>
+				<div className="recipe__main">
+					<h2>Ingredients</h2>
+					<table className="recipe__ingredients">
+						<tbody>
+							{recipe.ingredients.map((ingredient, index) => {
+								return (
+									<tr key={index}>
+										<td>{ingredient.quantity}</td>
+										<td>{ingredient.measurement}</td>
+										<td>{ingredient.item}</td>
+									</tr>
+								)
+							})}
+						</tbody>
+					</table>
+					<h2>Instructions</h2>
+					<ol className="recipe__instructions">
+						{recipe.instructions.map((step, index) => {
 							return (
-								<tr key={index}>
-									<td>{ingredient.quantity}</td>
-									<td>{ingredient.measurement}</td>
-									<td>{ingredient.item}</td>
-								</tr>
+								<li key={index}> {step.step}</li>
 							)
 						})}
-					</tbody>
-				</table>
-				<h2>Instructions</h2>
-				<ol className="recipe__instructions">
-					{recipe.instructions.map((step, index) => {
-						return (
-							<li key={index}> {step.step}</li>
-						)
-					})}
-				</ol>
-				<h2>Notes</h2>
-				<p className="recipe__notes">{recipe.notes}</p>
+					</ol>
+					<h2>Notes</h2>
+					<p className="recipe__notes">{recipe.notes}</p>
+				</div>
 			</div>
 		</div>
 	);
