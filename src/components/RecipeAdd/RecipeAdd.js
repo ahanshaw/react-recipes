9287cdc534fe53f825632d45ebfb1c8064679f98
Angie Hanshaw <ahanshaw@cliquestudios.com>
2021-11-17 11:44:37 -0600

    udpate recipe forms

--- src/components/RecipeAdd/RecipeAdd.js
+++ src/components/RecipeAdd/RecipeAdd.js
@@ -95,156 +95,159 @@ const RecipeAdd = () => {
     }
 
 	return (
-		<div className="recipe-add">
-			<form className="recipe-add__form" onSubmit={handleSubmit(onSubmit)}>
-				<div className="recipe-add__form__title">
-					<fieldset>
-						<label htmlFor="title">Title</label>
-						<input id="title" name="title" type="text" placeholder="Recipe title" {...register('title', { required: true })} onKeyUp = {(event) => handleTitle(event)} />
-						{errors.title && <p className="error">A recipe title is required.</p>}
-					</fieldset>
-				</div>
-				<div className="recipe-add__form__main">
-					<div className="recipe-add__form__section">
-						<legend>Ingredients</legend>
-						{ingredientsFields.map((field, index) => (
-							<fieldset className="recipe-add__form__ingredients" key={field.id}>
-								<div className="recipe-add__form__ingredients__quantity">
-									<label htmlFor={`quantity${index}`}>Quantity</label>
-									<input
-										type="text"
-										{...register(`ingredients.${index}.quantity`, {required: true})}
-										id={`quantity${index}`}
-										placeholder="1"
-										defaultValue={field.quantity}
-									/>
-								</div>
-								<div className="recipe-add__form__ingredients__measurement">
-									<label htmlFor={`measurement${index}`}>Measurement</label>
-									<input
-										{...register(`ingredients.${index}.measurement`)}
-										id={`measurement${index}`}
-										placeholder="cup"
-										defaultValue={field.measurement}
-									/>
-								</div>
-								<div className="recipe-add__form__ingredients__item">
-									<label htmlFor={`item${index}`}>Ingredient</label>
-									<input
-										{...register(`ingredients.${index}.item`, { required: true })}
-										id={`item${index}`}
-										placeholder="flour"
-										defaultValue={field.item}
-									/>
-								</div>
-								{ingredientsFields.length > 1 &&
-									<button className="remove" type="button" onClick={() => ingredientsRemove(index)}>&ndash;</button>
-								}
-								{errors.ingredients && <p className="error">Please add a quantity and an ingredient.</p>}
-							</fieldset>
-						))}
-						<button
-							type="button"
-							className="btn btn--add"
-							onClick={() => ingredientsAppend({ quantity: '', measurement: '', item: ''  })}>
-							Add Ingredient
-						</button>
-					</div>
-					<div className="recipe-add__form__section">
-						<legend>Instructions</legend>
-						{instructionsFields.map((field, index) => (
-							<div className="recipe-add__form__instructions" key={field.id}>
-								<fieldset className="recipe-add__form__instructions__item">
-									<label htmlFor={`instructions${index}`}>Step {index + 1}</label>
-									<textarea
-										{...register(`instructions.${index}.step`, { required: true })}
-										id={`instructions${index}`}
-										placeholder="Add at least one step."
-										defaultValue={field.step}
-									></textarea>
-								</fieldset>
-								{instructionsFields.length > 1 &&
-									<button className="remove" type="button" onClick={() => instructionsRemove(index)}>&ndash;</button>
-								}
-								{errors.instructions && <p className="error">Please include instructions.</p>}
+		<form className="recipe-form" onSubmit={handleSubmit(onSubmit)}>
+			<div className="recipe-form__title">
+				<fieldset className="fieldset-padded">
+					<label htmlFor="title">Title</label>
+					<input id="title" name="title" type="text" placeholder="Recipe title" {...register('title', { required: true })} onKeyUp = {(event) => handleTitle(event)} />
+					{errors.title && <p className="error">A recipe title is required.</p>}
+				</fieldset>
+			</div>
+
+			<div className="recipe-form__main">
+				<fieldset>
+					<legend>Ingredients</legend>
+					{ingredientsFields.map((field, index) => (
+						<div className="recipe-form__ingredient" key={field.id}>
+							<div className="recipe-form__ingredient__quantity">
+								<label htmlFor={`quantity${index}`}>Quantity</label>
+								<input
+									type="text"
+									{...register(`ingredients.${index}.quantity`, {required: true})}
+									id={`quantity${index}`}
+									placeholder="1"
+									defaultValue={field.quantity}
+								/>
 							</div>
-						))}
-						<button
-							type="button"
-							className="btn btn--add"
-							onClick={() => instructionsAppend({ step: '' })}>
-							Add Step
-						</button>
-					</div>
-					<fieldset>
-						<div className="recipe-add__form__notes">
-							<label htmlFor="notes">Notes</label>
-							<textarea id="notes" name="notes" placeholder="Optional notes." {...register('notes')}></textarea>
-						</div>
-					</fieldset>
-				</div>
-				<div className="recipe-add__form__side">
-					<fieldset>
-						<div className="recipe-add__form__cats__servings">
-							<label htmlFor="servings">Number of Servings</label>
-							<input id="servings" name="servings" placeholder="2" type="number" {...register('servings', { min: 1 })} />
-						</div>
-						{errors.servings && <p className="error">Must have at least one serving.</p>}
-					</fieldset>
-					<fieldset>
-						<div className="recipe-add__form__cats__cat">
-							<label htmlFor="category">Category</label>
-							<select id="category" name="category" {...register('category', { required: true })}>
-								<option value="">Choose a Category</option>
-								<option value="beverages">Beverages</option>
-								<option value="breads">Breads</option>
-								<option value="breakfast">Breakfast</option>
-								<option value="desserts">Desserts</option>
-								<option value="main">Main</option>
-								<option value="sauce">Sauce</option>
-								<option value="side">Side</option>
-								<option value="soup">Soup</option>
-								<option value="other">Other</option>
-							</select>
-						</div>
-						{errors.category && <p className="error">A recipe category is required.</p>}
-					</fieldset>
-					<fieldset>
-						<legend>Tags</legend>
-						{tagsFields.map((field, index) => (
-							<div className="recipe-add__form__tags" key={field.id}>
-								<div className="recipe-add__form__tags__tag">
-									<label className="sr-only" htmlFor={`tags${index}`}>Tag</label>
-									<input
-										{...register(`tags.${index}.tag`)}
-										id={`tags${index}`}
-										placeholder="optional tag"
-										defaultValue={field[index]}
-									/>
-								</div>
-								{tagsFields.length > 1 &&
-									<button className="remove" type="button" onClick={() => tagsRemove(index)}>
-										&ndash;
-									</button>
-								}
+							<div className="recipe-form__ingredient__measurement">
+								<label htmlFor={`measurement${index}`}>Measurement</label>
+								<input
+									{...register(`ingredients.${index}.measurement`)}
+									id={`measurement${index}`}
+									placeholder="cup"
+									defaultValue={field.measurement}
+								/>
 							</div>
-						))}
-						<button
-							type="button"
-							className="btn btn--add"
-							onClick={() => {
-								tagsAppend({ tag: '' });
-							}}
-						>
-							Add Tag
-						</button>
-					</fieldset>
-				</div>
-				<div className="recipe-add__form_submit">
-					<button className="btn btn--primary" type="submit">Add Recipe</button>
-				</div>
-			</form>
-		</div>
+							<div className="recipe-form__ingredient__item">
+								<label htmlFor={`item${index}`}>Ingredient</label>
+								<input
+									{...register(`ingredients.${index}.item`, { required: true })}
+									id={`item${index}`}
+									placeholder="flour"
+									defaultValue={field.item}
+								/>
+							</div>
+							{ingredientsFields.length > 1 &&
+								<div className="recipe-form__ingredient__remove">
+									<button className="remove" type="button" onClick={() => ingredientsRemove(index)}>+</button>
+								</div>
+							}
+							{errors.ingredients && <p className="error">Please add a quantity and an ingredient.</p>}
+						</div>
+					))}
+					<button
+						type="button"
+						className="btn btn--add"
+						onClick={() => ingredientsAppend({ quantity: '', measurement: '', item: ''  })}>
+						Add Ingredient
+					</button>
+				</fieldset>
+
+				<fieldset>
+					<legend>Instructions</legend>
+					{instructionsFields.map((field, index) => (
+						<div className="recipe-form__instruction" key={field.id}>
+							<div className="recipe-form__instruction__step">
+								<label htmlFor={`instructions${index}`}>Step {index + 1}</label>
+								<textarea
+									{...register(`instructions.${index}.step`, { required: true })}
+									id={`instructions${index}`}
+									placeholder="Add at least one step."
+									defaultValue={field.step}
+								></textarea>
+							</div>
+							{instructionsFields.length > 1 &&
+								<div className="recipe-form__instruction__remove">
+									<button className="remove" type="button" onClick={() => instructionsRemove(index)}>+</button>
+								</div>
+							}
+							{errors.instructions && <p className="error">Please include instructions.</p>}
+						</div>
+					))}
+					<button
+						type="button"
+						className="btn btn--add"
+						onClick={() => instructionsAppend({ step: '' })}>
+						Add Step
+					</button>
+				</fieldset>
+
+				<fieldset className="fieldset-padded">
+					<label htmlFor="notes">Notes</label>
+					<textarea id="notes" name="notes" placeholder="Optional notes." {...register('notes')}></textarea>
+				</fieldset>
+			</div>
+
+			<div className="recipe-form__side">
+				<fieldset className="fieldset-padded">
+					<label htmlFor="servings">Number of Servings</label>
+					<input id="servings" name="servings" placeholder="2" type="number" {...register('servings', { min: 1 })} />
+					{errors.servings && <p className="error">Must have at least one serving.</p>}
+				</fieldset>
+
+				<fieldset className="fieldset-padded">
+					<label htmlFor="category">Category</label>
+					<select id="category" name="category" {...register('category', { required: true })}>
+						<option value="">Choose a Category</option>
+						<option value="beverages">Beverages</option>
+						<option value="breads">Breads</option>
+						<option value="breakfast">Breakfast</option>
+						<option value="desserts">Desserts</option>
+						<option value="main">Main</option>
+						<option value="sauce">Sauce</option>
+						<option value="side">Side</option>
+						<option value="soup">Soup</option>
+						<option value="other">Other</option>
+					</select>
+					{errors.category && <p className="error">A recipe category is required.</p>}
+				</fieldset>
+
+				<fieldset>
+					<legend>Tags</legend>
+					{tagsFields.map((field, index) => (
+						<div className="recipe-form__tag" key={field.id}>
+							<div className="recipe-form__tag__item">
+								<label className="sr-only" htmlFor={`tags${index}`}>Tag</label>
+								<input
+									{...register(`tags.${index}.tag`)}
+									id={`tags${index}`}
+									placeholder="optional tag"
+									defaultValue={field[index]}
+								/>
+							</div>
+							{tagsFields.length > 1 &&
+								<div className="recipe-form__tag__remove">
+									<button className="remove" type="button" onClick={() => tagsRemove(index)}>+</button>
+								</div>
+							}
+						</div>
+					))}
+					<button
+						type="button"
+						className="btn btn--add"
+						onClick={() => {
+							tagsAppend({ tag: '' });
+						}}
+					>
+						Add Tag
+					</button>
+				</fieldset>
+			</div>
+			<div className="recipe-form__submit">
+				<button className="btn btn--primary" type="submit">Add Recipe</button>
+				<Link className="btn btn--secondary" to={`/account/dashboard`}>Cancel</Link>
+			</div>
+		</form>
 	);
 }
 
