e0df05c23165da56cf692c091cfb1f57d5fb6bf1
Angie Hanshaw <ahanshaw@cliquestudios.com>
2021-11-17 21:11:40 -0600

    add favorites

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -8,10 +8,11 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 export default function UserDashboard() {
 	const [user, loading] = useAuthState(auth);
 	const [recipes, setRecipes] = useState([]);
+	const [favorites, setFavorites] = useState([]);
 
 	useEffect(() => {
-		database.ref('recipes').once('value', function (snapshot) {
-			if (user && loading === false) {
+		if (user && loading === false) {
+			database.ref('recipes').once('value', function (snapshot) {
 				let recipeArr = [];
 				snapshot.forEach(recipe => {
 					if (recipe.val().user === user.uid) {
@@ -24,8 +25,20 @@ export default function UserDashboard() {
 					}
 				});
 				setRecipes(recipeArr);
-			}
-		});
+			});
+		
+			database.ref('users').child(user.uid).child('favorites').once('value', function (snapshot) {
+				let recipeArr = [];
+				snapshot.forEach(recipe => {
+					let userRecipe = {
+						'title': recipe.val().title,
+						'key': recipe.val().key,
+					}
+					recipeArr.push(userRecipe);
+				});
+				setFavorites(recipeArr);
+			});
+		}
 	}, [user, loading]);
 
 	if (loading) {
@@ -37,7 +50,7 @@ export default function UserDashboard() {
         );
     }
 
-	if (user && recipes.length > 0) {
+	if (user) {
 		return (
 			<div className="dashboard">
 				<div className="dashboard__title">
@@ -51,33 +64,31 @@ export default function UserDashboard() {
 				</div>
 				<div className="dashboard__content">
 					<h2>My Recipes</h2>
-					{recipes
+					{recipes.length > 0 &&
+						recipes
 						.sort((a, b) => a.added < b.added ? 1:-1)
 						.map(recipe => {
-						return (
-							<p key={recipe.key}><Link className="link" to={`/recipe/${recipe.key}/${recipe.title.toLowerCase().replace(/\s/g, '-')}`}>{recipe.title}</Link></p>
-						)
-					})}
-				</div>
-			</div>
-        );
-	}
-	
-	if (user && recipes.length < 1) {
-		return (
-			<div className="dashboard">
-				<div className="dashboard__title">
-					<h1>My Dashboard</h1>
-				</div>
-				<div className="dashboard__side">
-					<p><Link className="btn btn--primary" to={`/add`}>Add a recipe</Link></p>
-					<p><button className="link" onClick={logout}>
-						Log Out
-					</button></p>
-				</div>
-				<div className="dashboard__content">
-					<h2>My Recipes</h2>
-					<p>Looks like you haven&#8217;t added any recipes yet.</p>
+							return (
+								<p key={recipe.key}><Link className="link" to={`/recipe/${recipe.key}/${recipe.title.toLowerCase().replace(/\s/g, '-')}`}>{recipe.title}</Link></p>
+							)
+						})
+					}
+					{recipes.length < 1 &&
+						<p>Looks like you haven&#8217;t added any recipes yet.</p>
+					}
+
+					<h2>My Favorites</h2>
+					{favorites.length > 0 &&
+						favorites
+						.map(favorite => {
+							return (
+								<p key={favorite.key}><Link className="link" to={`/recipe/${favorite.key}/${favorite.title.toLowerCase().replace(/\s/g, '-')}`}>{favorite.title}</Link></p>
+							)
+						})
+					}
+					{favorites.length < 1 &&
+						<p>Looks like you haven&#8217;t favorited any recipes yet.</p>
+					}
 				</div>
 			</div>
         );
